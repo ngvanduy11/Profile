eba2e29fc33b1b8539a57a517761fda14a59924d
ngvanduy11 <ngvanduy101@gmail.com>
2026-07-18 00:37:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,54 @@ document.getElementById("music").onclick=()=>{
 // ===== Particles =====
 
 tsParticles.load("particles",{
+const USER_ID = "257851469531840514";
 
+async function loadDiscord(){
+
+    try{
+
+        const res = await fetch(
+            `https://api.lanyard.rest/v1/users/${USER_ID}`
+        );
+
+        const data = (await res.json()).data;
+
+        const dot = document.getElementById("dot");
+        const status = document.getElementById("statusText");
+        const activity = document.getElementById("activity");
+
+        status.innerText = data.discord_status;
+
+        if(data.discord_status==="online")
+            dot.style.background="#43b581";
+
+        if(data.discord_status==="idle")
+            dot.style.background="#faa61a";
+
+        if(data.discord_status==="dnd")
+            dot.style.background="#f04747";
+
+        if(data.discord_status==="offline")
+            dot.style.background="gray";
+
+        if(data.activities.length){
+
+            activity.innerText =
+            data.activities[0].name;
+
+        }
+
+    }catch(e){
+
+        console.log(e);
+
+    }
+
+}
+
+loadDiscord();
+
+setInterval(loadDiscord,15000);
     background:{
         color:"transparent"
     },
